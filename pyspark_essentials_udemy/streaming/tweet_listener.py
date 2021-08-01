f2800668f6055c4bda7bde3fe810b1110018ac1b
ahowe42 <ahowe42@protonmail.com>
2021-08-01 21:32:15 +0100

    Finished for now playing around with streaming tweets. Not sure if my delimiter for splitting is wrong, but getting some weird stuff in the usernames. It works, though.

--- pyspark_essentials_udemy/streaming/tweet_listener.py
+++ pyspark_essentials_udemy/streaming/tweet_listener.py
@@ -4,65 +4,93 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 import socket
 import json
-import getpass
+import pdb
 
+def GetKeys(fileName):
+	'''
+	Load from a file the access and api keys needed, then return as a 
+	list of access token, access secret, api key, api secret.
+	'''
+	# read the file and get the first 4 lines
+	with open(fileName, 'rt') as f:
+		keys = f.readlines()[:4]
+	# drop the header in each row
+	keys = [key.split(':')[1].strip() for key in keys]
+	return keys
 
 # handler class
 class TweetsListener(StreamListener):
 	def __init__(self, csocket):
 		self.client_socket = csocket
-	def onData(self, data):
+		self.sep = '¬'
+
+	def on_data(self, data):
 		try:
-			msgTxt = json.loads(data) 
-			msgTxt = msg['text'].encode('utf-8')
-			print(msgTxt)
-			self.client_socket.send(msgTxt)
+			# read the incoming data ...
+			msg = json.loads(data)
+			#pdb.set_trace()
+			msgTxt = msg['text']
+			msgUser = msg['user']['screen_name']
+			msgDat = msg['created_at']
+			sendMe = (msgUser + self.sep + msgDat + self.sep + msgTxt).encode('utf-8')
+			print(sendMe)
+			# ... and send it
+			self.client_socket.send(sendMe)
 		except BaseException as err:
 			print('Error: %s'%err)
-		# done
 		return True
- 
-	def onError(self, status):
+
+	def on_error(self, status):
 		print(status)
 		return True
 
-# setup the socket object
-sock = socket.socket()
-host = '127.0.0.1' 
-port = 4205
-sock.bind((host, port))
-print('Listening on %s:%d'%(host, port))
+# ask user for topic to filter
+topic = input('What topic to filter tweets?')
 
-# listen
+# get the keys
+print('Getting access keys...')
 try:
-	sock.listen(5) # size of backlog of pending requests
-	clientSocket, address = sock.accept()	
-except KeyboardInterrupt as err:
+	keys = GetKeys('./keys.txt')
+	print('Keys %s'%keys)
+	accessToken, accessSecret, consumerKey, consumerSecret = keys
+except Exception as err:
+	print('Error getting keys: %s'%err)
 	sock.close()
 	print('Closed')
 else:
-	print('Received request from %s:%d, connected'%(address[0], address[1]))
-
-	# get the tokens and setup twitter authorization
-	accessToken = getpass.getpass('Access Token')
-	accessSecret = getpass.getpass('Access Secret')
-	consumerKey = getpass.getpass('API Key')
-	consumerSecret = getpass.getpass('API Secret')
+	# setup the socket object
+	sock = socket.socket()
+	host = '127.0.0.1' 
+	port = 4208
+	sock.bind((host, port))
+	print('Listening on %s:%d'%(host, port))
+	
+	# listen
 	try:
-		auth = OAuthHandler(consumerKey, consumerSecret)
-		auth.set_access_token(accessToken, accessSecret)
-	except Exception as err:
-		print('Authorization error: %s'%err)
-		clientSocket.close()
-		sock.close
+		sock.listen(5) # size of backlog of pending requests
+		clientSocket, address = sock.accept()
+	except KeyboardInterrupt as err:
+		sock.close()
+		print('Closed')
 	else:
+		print('Received request from %s:%d, connected'%(address[0], address[1]))
+
+		# get the tokens and setup twitter authorization
 		try:
-			# listen to twitter, but only partially listen
-			twitterStream = Stream(auth, TweetsListener(clientSocket))
-			twitterStream.filter(track=['NASA'])
+			auth = OAuthHandler(consumerKey, consumerSecret)
+			auth.set_access_token(accessToken, accessSecret)
 		except Exception as err:
-			print('Error: %s'%err)
-			# close the connection
+			print('Authorization error: %s'%err)
 			clientSocket.close()
-			sock.close()
-			print('Closed connection')
+			sock.close
+		else:
+			try:
+				# listen to twitter, but only partially listen
+				twitterStream = Stream(auth, TweetsListener(clientSocket))
+				twitterStream.filter(track=[topic])
+			except Exception as err:
+				print('Error: %s'%err)
+				# close the connection
+				clientSocket.close()
+				sock.close()
+				print('Closed connection')
